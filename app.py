52a08eecd6dcd22fcaffcf27d2cb51260e35a814
Darsh <yadavabheesht@gmail.com>
2026-06-05 17:42:54 +0530

    added favs

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask,render_template,request,redirect,url_for,flash,session
 import requests
 import backend as re
+from datetime import timedelta
 app=Flask(__name__)
 app.secret_key = "fhgztghgdh"
+
+    
+
+app.permanent_session_lifetime=timedelta(days=30)
 @app.route("/",methods=["POST","GET"])
 def home():
     if request.method=="POST":
@@ -14,9 +19,9 @@ def home():
 @app.route("/pokemon/<pname>")
 def info(pname):
     
-    signal, idd, weight, height = re.main(pname)
+    signal, idd, weight, height , img = re.main(pname)
     if(signal=="green"):
-        return render_template("info.html",pname=pname,height=height,weight=weight,id=idd)
+        return render_template("info.html",pname=pname,height=height,weight=weight,id=idd,img=img)
     elif(signal=="red"):
         flash(f"Pokemon not found in database perhapes you meant {re.ai_module(pname)}?","warning")
         return redirect(url_for("home"))
@@ -31,6 +36,7 @@ def info(pname):
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent=True
         username = request.form['username']
         session['username'] = username  
         return redirect(url_for("home"))
@@ -39,6 +45,7 @@ def login():
     
 @app.route("/profile/<profile>")
 def pro(profile):
+    
     return render_template("user.html",usr=profile)
 
 @app.route("/delete/<uname>",methods=["POST"])
@@ -46,6 +53,25 @@ def delete_username(uname):
     session.pop('username',None)
     flash(f"{uname} have been logged out ")
     return redirect(url_for("home"))
+
+
+@app.route("/fav/<pname>",methods=["POST"])
+def fav_pokemon(pname):
+    if 'fav_pokemon' not in session:
+     session['fav_pokemon']=[]
+    if(request.method=="POST"):
+        if pname not in session['fav_pokemon']:
+
+            session['fav_pokemon'].append(pname)
+            session.modified=True
+            flash(f"{pname} is added to your fav")
+            return redirect(url_for("info",pname=pname))
+        else:
+            flash(f"{pname} is already in your fav")
+            return redirect(url_for("info",pname=pname))
+
+
+    
 
 
 
--- backend.py
+++ backend.py
@@ -38,12 +38,14 @@ def main(pname):
     data=requests.get(url)
     if(data.status_code==200):
         signal="green"
+        
         response=data.json()
+        image_url=response.get('sprites', {}).get('other', {}).get('official-artwork', {}).get('front_default')
         idd=response.get('id')
 
         hight=response.get('height')
         weight=response.get('weight')/10
-        return signal,idd,weight,hight
+        return signal,idd,weight,hight,image_url
 
 
     elif(data.status_code==404):
@@ -51,10 +53,12 @@ def main(pname):
         idd=None
         weight=None
         hight=None
-        return signal,idd,weight,hight
+        image_url=None
+        return signal,idd,weight,hight,image_url
     else:
         signal="black"
         idd=None
         weight=None
         hight=None
-        return signal,idd,weight,hight
+        image_url=None
+        return signal,idd,weight,hight,image_url
