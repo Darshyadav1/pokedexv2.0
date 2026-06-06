fe2d74cd88884ea6396db6b7818d209b872eb58d
Darsh <yadavabheesht@gmail.com>
2026-06-06 08:12:04 +0530

    added option to delete from fav

--- app.py
+++ app.py
@@ -16,18 +16,25 @@ def home():
     else:
      return render_template("home.html")
 
-@app.route("/pokemon/<pname>")
+@app.route("/pokemon/<pname>",methods=['POST','GET'])
 def info(pname):
-    
-    signal, idd, weight, height , img = re.main(pname)
-    if(signal=="green"):
-        return render_template("info.html",pname=pname,height=height,weight=weight,id=idd,img=img)
-    elif(signal=="red"):
-        flash(f"Pokemon not found in database perhapes you meant {re.ai_module(pname)}?","warning")
-        return redirect(url_for("home"))
+    if request.method=="POST":
+        return redirect(url_for("delete_fav",pname=pname))
+
     else:
-        flash("poki api down","warning")
-        return redirect(url_for("home"))
+        signal, idd, weight, height , img = re.main(pname)
+        if(signal=="green"):
+            return render_template("info.html",pname=pname,height=height,weight=weight,id=idd,img=img)
+        elif(signal=="red"):
+            flash(f"Pokemon not found in database perhapes you meant {re.ai_module(pname)}?","warning")
+            return redirect(url_for("home"))
+        else:
+            flash("poki api down","warning")
+            return redirect(url_for("home"))
+    
+
+    
+
 
 
     
@@ -47,6 +54,7 @@ def login():
 def pro(profile):
     
     return render_template("user.html",usr=profile)
+    
 
 @app.route("/delete/<uname>",methods=["POST"])
 def delete_username(uname):
@@ -57,6 +65,7 @@ def delete_username(uname):
 
 @app.route("/fav/<pname>",methods=["POST"])
 def fav_pokemon(pname):
+    session.permanent=True
     if 'fav_pokemon' not in session:
      session['fav_pokemon']=[]
     if(request.method=="POST"):
@@ -69,6 +78,23 @@ def fav_pokemon(pname):
         else:
             flash(f"{pname} is already in your fav")
             return redirect(url_for("info",pname=pname))
+        
+
+@app.route("/deletep/<pname>", methods=["POST"])
+def delete_fav(pname):
+    
+    if 'fav_pokemon' in session and isinstance(session['fav_pokemon'], list):
+        
+        
+        session['fav_pokemon'] = [p for p in session['fav_pokemon'] if p != pname]
+        
+        
+        session.modified = True
+        flash(f"{pname} removed from favorites!","info")
+    else:
+        flash("No favorites list found.")
+
+    return redirect(url_for("info", pname=pname))
 
 
     
